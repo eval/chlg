0fcbcf4c1128cb1db831addcd8cd0a1c283cbb34
Gert Goet <gert@thinkcreate.nl>
2014-02-10 00:21:49 +0100

    Core-async for handling download callbacks

--- src/chlog/core.cljs
+++ src/chlog/core.cljs
@@ -1,5 +1,7 @@
 (ns chlog.core
-  (:use [cljs.nodejs :only [require]]))
+  (:require [cljs.core.async :as async :refer [<! >! chan close! put! get! timeout]]
+            [cljs.nodejs :as node])
+  (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
 (defn print-usage []
   (println "
@@ -11,25 +13,30 @@
     # 0.0.1 / 2014-02-9
 
     Initial release
-"))
+")
+)
 
 (defn print-usage? [args]
   (or
     (not (seq args))
     (= "-h" (first args))))
 
-(defn gh-request []
-  (let [ps (require "child_process")
-        url "https://api.github.com/repos/bundler/bundler/git/trees/master"]
-    (.exec ps (str "curl -s " url) #(println (repos-files (JSON/parse %2))))))
+(def gh-token
+  node/process.env.GH_TOKEN)
 
-(def gh-files-json
-  (JSON/parse "{\"tree\": [{\"path\":\"spec\"},{\"path\":\"lib\"}]}"))
+(defn gh-request [path]
+  (let [ps (node/require "child_process")
+        url (str "https://api.github.com/" path "?access_token=" gh_token)
+        c (chan)]
+    (.exec ps (str "curl -s " url) #(put! c (JSON/parse %2)))
+  c))
+
+(defn get-tree [repos]
+  (let [path (str "repos/" repos "/git/trees/master")]
+    (gh-request path)))
 
 (defn repos-files [json]
   (map #(aget % "path") (aget json "tree")))
-  ;(.. (JSON/parse gh-files-json) -tree))
-  ;(map #(.-path %) (.-tree gh-files-json)))
 
 (defn changelog-path [repos])
 
@@ -37,14 +44,14 @@
   ;(println (repos-files (gh-request))))
   (println (gh-request)))
 
-(defn print-changelog2 [repos]
-  (let [ps (require "child_process")
-        repos-url (str "https://raw.github.com/" repos "/master/railties/CHANGELOG.md")]
-    (.exec ps (str "curl -s " repos-url) #(println %2))))
+(defn print-root-files [repos]
+  (let [repos-root-files (get-tree repos)]
+    (go
+      (println (repos-files (<! repos-root-files))))))
 
 (defn start [& args]
   (if (print-usage? args)
     (print-usage)
-    (print-changelog (first args))))
+    (print-root-files (first args))))
 
 (set! *main-cli-fn* start)
