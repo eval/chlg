c4c51eb6e619214109bdb4bc48f3e67072aae48f
Gert Goet <gert@thinkcreate.nl>
2014-02-09 22:48:08 +0100

    Async get of repos-tree

--- src/chlog/core.cljs
+++ src/chlog/core.cljs
@@ -18,7 +18,26 @@
     (not (seq args))
     (= "-h" (first args))))
 
+(defn gh-request []
+  (let [ps (require "child_process")
+        url "https://api.github.com/repos/bundler/bundler/git/trees/master"]
+    (.exec ps (str "curl -s " url) #(println (repos-files (JSON/parse %2))))))
+
+(def gh-files-json
+  (JSON/parse "{\"tree\": [{\"path\":\"spec\"},{\"path\":\"lib\"}]}"))
+
+(defn repos-files [json]
+  (map #(aget % "path") (aget json "tree")))
+  ;(.. (JSON/parse gh-files-json) -tree))
+  ;(map #(.-path %) (.-tree gh-files-json)))
+
+(defn changelog-path [repos])
+
 (defn print-changelog [repos]
+  ;(println (repos-files (gh-request))))
+  (println (gh-request)))
+
+(defn print-changelog2 [repos]
   (let [ps (require "child_process")
         repos-url (str "https://raw.github.com/" repos "/master/railties/CHANGELOG.md")]
     (.exec ps (str "curl -s " repos-url) #(println %2))))
