1f655269aebe6f05dc64f2f90a3cac52c5f3c18c
Gert Goet <gert@thinkcreate.nl>
2014-02-11 22:19:29 +0100

    Show changelog file

--- src/chlog/core.cljs
+++ src/chlog/core.cljs
@@ -1,6 +1,7 @@
 (ns chlog.core
   (:require [cljs.core.async :as async :refer [<! >! chan close! put! get! timeout]]
-            [cljs.nodejs :as node])
+            [cljs.nodejs :as node]
+            [clojure.string :as string])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
 (defn print-usage []
@@ -24,37 +25,44 @@
 (def gh-token
   node/process.env.GH_TOKEN)
 
-(defn gh-request [path]
+(defn gh-request [path & [format]]
   (let [ps (node/require "child_process")
-        url (str "https://api.github.com/" path "?access_token=" gh_token)
+        format (or format :json)
+        headers {:accept (str "application/vnd.github.3." (name format))}
+        auth_header (if gh-token {:authorization (str "token " gh-token)} {})
+        curl_headers (string/join " " (map (fn [[k v]] (str "-H '" (name k) ": " v "'")) (merge headers auth_header)))
+        url (str "https://api.github.com/" path)
         c (chan)]
-    (.exec ps (str "curl -s " url) #(put! c (JSON/parse %2)))
+    (.exec ps (str "curl -s " curl_headers " " url) #(put! c %2))
   c))
 
 (defn get-tree [repos]
   (let [path (str "repos/" repos "/git/trees/master")]
     (gh-request path)))
 
-(defn repos-files [json]
-  (map #(aget % "path") (aget json "tree")))
+(defn get-content [repos file]
+  (let [path (str "repos/" repos "/contents/" file)]
+    (gh-request path :raw)))
+
+(defn repos-files [response]
+  (let [json (JSON/parse response)]
+    (map #(aget % "path") (aget json "tree"))))
 
 (defn changelog-path [files]
-  "Select file that is considered the changelog"
   (let [changelog-re #"(?i)changelog|history"]
     (first (filter #(re-find changelog-re %) files))))
 
 (defn print-changelog [repos]
-  ;(println (repos-files (gh-request))))
-  (println (gh-request)))
-
-(defn print-root-files [repos]
-  (let [repos-root-files (get-tree repos)]
-    (go
-      (println (changelog-path (repos-files (<! repos-root-files)))))))
+  (go
+    (let [repos-root-files (get-tree repos)
+          path (changelog-path (repos-files (<! repos-root-files)))
+          changelog-content (get-content repos path)]
+      (println (<! changelog-content)))))
 
 (defn start [& args]
   (if (print-usage? args)
     (print-usage)
-    (print-root-files (first args))))
+    (let [[repos] args]
+      (print-changelog repos))))
 
 (set! *main-cli-fn* start)
