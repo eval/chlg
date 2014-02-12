d7915dbade993ee662ab02e486e5c3e12c92336d
Gert Goet <gert@thinkcreate.nl>
2014-02-12 13:01:48 +0100

    Search for repository if needed

--- src/chlog/core.cljs
+++ src/chlog/core.cljs
@@ -1,5 +1,5 @@
 (ns chlog.core
-  (:require [cljs.core.async :as async :refer [<! >! chan close! put! get! timeout]]
+  (:require [cljs.core.async :as async :refer [<! >! chan close! put! take! timeout]]
             [cljs.nodejs :as node]
             [clojure.string :as string])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
@@ -33,11 +33,15 @@
         curl_headers (string/join " " (map (fn [[k v]] (str "-H '" (name k) ": " v "'")) (merge headers auth_header)))
         url (str "https://api.github.com/" path)
         c (chan)]
-    (.exec ps (str "curl -s " curl_headers " " url) #(put! c %2))
+    (.exec ps (str "curl -s " curl_headers " '" url "'") #(put! c %2))
   c))
 
 (defn get-tree [repos]
   (let [path (str "repos/" repos "/git/trees/master")]
+    (gh-request path)))
+
+(defn get-search [q]
+  (let [path (str "search/repositories?per_page=1&q=" q)]
     (gh-request path)))
 
 (defn get-content [repos file]
@@ -48,6 +52,10 @@
   (let [json (JSON/parse response)]
     (map #(aget % "path") (aget json "tree"))))
 
+(defn filter-repos-full-name [search-response]
+  (let [json (JSON/parse search-response)]
+    (aget (first (aget json "items")) "full_name")))
+
 (defn changelog-path [files]
   (let [changelog-re #"(?i)changelog|history"]
     (first (filter #(re-find changelog-re %) files))))
@@ -57,12 +65,20 @@
     (let [repos-root-files (get-tree repos)
           path (changelog-path (repos-files (<! repos-root-files)))
           changelog-content (get-content repos path)]
+      (println (str "Changelog of repository '" repos "':"))
       (println (<! changelog-content)))))
+
+(defn search-full-repos-name [q]
+  (go
+    (let [request (get-search q)]
+      (filter-repos-full-name (<! request)))))
 
 (defn start [& args]
   (if (print-usage? args)
     (print-usage)
-    (let [[repos] args]
-      (print-changelog repos))))
+    (go
+      (let [[repos] args
+            full-name (if (re-find #"/" repos) repos (<! (search-full-repos-name repos)))]
+        (print-changelog full-name)))))
 
 (set! *main-cli-fn* start)
